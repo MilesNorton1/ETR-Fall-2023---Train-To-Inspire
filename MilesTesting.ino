c0fa64369f46ca22a7a1c1e9dffac1a6fbcfab25
MilesNorton1 <150288720+MilesNorton1@users.noreply.github.com>
2023-11-27 14:48:39 -0600

    Update MilesTesting.ino

--- MilesTesting.ino
+++ MilesTesting.ino
@@ -1,6 +1,9 @@
-const int numLeds = 3;
-const int ledPins[] = {2, 3, 4};
-const int pirPins[] = {5, 6, 7};
+const int numLeds = 10;
+const int ledPins[] = {22, 23, 24, 25, 26, 27, 28, 29, 30, 31};
+const int pirPins[] = {32, 33, 34, 35, 36, 37, 38, 39, 40, 41};
+
+int numWhacksToWin = 5;  // Set the number of whacks needed to win the game
+int whacksCounter = 0;   // Counter to keep track of whacks
 
 int previousLedIndex = -1;
 
@@ -20,6 +23,12 @@ void setup() {
 }
 
 void loop() {
+  // Check if the game is done
+  if (whacksCounter >= numWhacksToWin) {
+    // Game is done, you can add additional logic here if needed
+    return;
+  }
+
   // Generate a random LED index different from the previous one
   int randomLedIndex = generateRandomLedIndex();
 
@@ -34,6 +43,9 @@ void loop() {
 
   // Update the previous LED index
   previousLedIndex = randomLedIndex;
+
+  // Increment the whacks counter
+  whacksCounter++;
 
   // Delay before the next round
   delay(1000);
